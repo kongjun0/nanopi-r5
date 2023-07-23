8525d523fb4e9b6c0aa6f4d3c967a18c83b89ddc
kongjun0 <33769434+kongjun0@users.noreply.github.com>
2023-07-23 15:01:41 +0800

    Update make_debian_img.sh

--- debian/make_debian_img.sh
+++ debian/make_debian_img.sh
@@ -351,14 +351,12 @@ file_apt_sources() {
 	# For information about how to configure apt package sources,
 	# see the sources.list(5) manual.
 
-	deb http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
-        deb-src http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
-        deb http://mirrors.aliyun.com/debian-security/ bullseye-security main
-        deb-src http://mirrors.aliyun.com/debian-security/ bullseye-security main
-        deb http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
-        deb-src http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
-        deb http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
-        deb-src http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+	deb http://mirrors.cloud.tencent.com/debian bullseye main contrib non-free
+        deb-src http://mirrors.cloud.tencent.com/debian bullseye main contrib non-free
+        deb http://mirrors.cloud.tencent.com/debian-security bullseye-security main contrib non-free
+        deb-src http://mirrors.cloud.tencent.com/debian-security bullseye-security main contrib non-free
+        deb http://mirrors.cloud.tencent.com/debian bullseye-updates main contrib non-free
+        deb-src http://mirrors.cloud.tencent.com/debian bullseye-updates main contrib non-free
 	EOF
 }
 
